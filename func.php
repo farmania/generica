8985d90cd5dda393ebd4a966ad1726e6a50198a6
farmania <76524866+farmania@users.noreply.github.com>
2021-06-21 15:44:50 +0200

    base

---
+++ func.php
@@ -0,0 +1,36 @@
+<?php
+
+use Tygh\Registry;
+use Tygh\Embedded;
+use Tygh\Session;
+
+if (!defined('BOOTSTRAP')) { die('Access denied'); }
+
+function fn_cp_gestpay_integration_install(){
+    fn_cp_gestpay_integration_uninstall();
+    $_data = array(
+        'processor' => 'GestPay Gateway',
+        'processor_script' => 'cp_gestpay_integration.php',
+        'processor_template' => 'views/orders/components/payments/cc_outside.tpl',// default
+        'template' => 'views/orders/components/payments/cc_outside.tpl',
+        'admin_template' => 'cp_gestpay_integration.tpl', // with config in admin side
+        'callback' => 'N',
+        'type' => 'P',
+        'addon' => 'cp_gestpay_integration'
+    );
+    
+    db_query("INSERT INTO ?:payment_processors ?e", $_data);
+}
+
+
+function fn_cp_gestpay_integration_uninstall(){
+    db_query("DELETE FROM ?:payment_processors where processor_script = ?s", "cp_gestpay_integration.php");
+}
+
+
+function fn_cp_gestpay_integration_save_log($type, $action, $data, $user_id, &$content, $event_type, $object_primary_keys)
+{
+    if ($type == 'gestpay_integration') {
+        $content = $data;
+    }
+}
